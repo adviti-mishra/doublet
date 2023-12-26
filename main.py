d0eb6b64692b924a8b494370a8b786dcb7081df5
adviti <adviti@umich.edu>
2023-12-26 11:56:57 +0530

    Word entered by the user is now a valid word in the game

--- main.py
+++ main.py
@@ -15,13 +15,37 @@ class Game:
         for row in csvreader:
             self.rows.append(row)
     
+    def is_word_valid(self):
+        # TODO: change
+        return True
+    
     def manage_rounds(self):
+
         # code for round 1 only 
         print("ROUND 1 ")
         # start word
         print(f"START: {self.rows[0][0]}")
         # end word
         print(f"END: {self.rows[0][1]}")
+
+        # user input word
+        word = input("Enter next word")
+        # validate it
+        # 1) word is a valid English word
+        # 2) word differs from previous word by 1 letter 
+        # 3) word has the same letters as the start word
+        valid_word = self.is_word_valid(word)
+        # get user input word until valid word encountered
+        while not valid_word:
+            word = input("Please enter a valid word. Your word needs to be a valid English word and needs to differ from the previous word by at most 1 letter ")
+            # validate it
+            valid_word = self.is_word_valid(word)
+
+        # now, word is a valid word in the game
+        
+        
+        
+
 
 
 
