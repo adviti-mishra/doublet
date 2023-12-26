a5a6e8b0d9671e2f9d7cd7a0c34bb9383d7619f8
adviti <adviti@umich.edu>
2023-12-26 11:36:11 +0530

    Rough pseudocode as comments in main function

---
+++ main.py
@@ -0,0 +1,13 @@
+def main():
+    # loop through csv file 
+    # print start and end word
+    # while not end_game
+    # for each word
+    # check if word is valid and if differs from previous word by only 1 letter
+    # if the word is now the end word, set end_game to true
+    # if the word is q, set end_game to true
+    # print number of intermediate words and game status
+    pass
+
+if __name__=='__main__':
+    main()
